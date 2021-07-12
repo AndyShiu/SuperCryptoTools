d3147e92f88dfcbd4015bf27c4393f6c1bece222
AndyShiu <andy19910823@gmail.com>
2021-07-12 09:48:47 +0800

    [2021/07/12] 批次處理系使交易紀錄

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 var remoteIpAddress='https://35.206.246.81';
-
+//var remoteIpAddress='https://localhost';
 var mdDefData = 
 [                                         
   {                                       
